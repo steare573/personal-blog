dd67e0ca1d73e1b3e1b11f82fe705dae340d85ec
Sean Teare <steare573@gmail.com>
2017-06-10 01:45:15 -0400

    <ST>push to root directory if index.html is manually entered as the route

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,11 @@
 import ReactGA from 'react-ga';
 import { config } from 'config';
+import { browserHistory } from 'react-router';
+// this is an obvious hack, but trying to just get this done quickly
+// TODO: Find a more elegant solution to send index.html to the root path
+if (window && window.location.pathname === '/index.html') {
+  browserHistory.push('/');
+}
 
 ReactGA.initialize(config.googleAnalyticsId);
 
